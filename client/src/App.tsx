e84f6bfb2be46d35a4c07b488e9d111d97d2a2f8
mindelias@gmail.com <mindelias@gmail.com>
2020-04-02 02:46:24 +0100

    set up express

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,50 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
 import logo from './logo.svg';
 import './App.css';
+import ContactInfo from './ContactInfo';
 
-const App = () => {
+const contactQuery = gql`
+  query contacts($contactID: String!) {
+    allContacts {
+      id
+      firstName
+      ...ContactInfo
+    }
+    contactByID(contactID: $contactID) {
+      ...contactInfo
+    }
+  }
+  ${ContactInfo.fragments.contact}
+`;
+
+function App() {
+  const { data, loading, error } = useQuery(contactQuery, {
+    variables: { contactID: '14cc7f46-23a0-4017-bdef-6801e104090a' },
+  });
+
+  if (error) {
+    return <>'There was an error'</>;
+  }
+
+  if (loading) {
+    return <>'Loading'</>;
+  }
+
+  const contacts = data.allContacts.map(
+    (contact: { id: string; firstName: string }) => {
+      return <li key={contact.id}>{contact.firstName}</li>;
+    }
+  );
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>{contacts}</ul>
+        <ContactInfo contacts={data.allContacts} />
       </header>
     </div>
   );
